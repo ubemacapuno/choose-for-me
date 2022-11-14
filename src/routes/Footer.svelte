7c35f54e3915d7ffaaf4908448dafaf216f5d14d
corey <damoclescj@gmail.com>
2022-11-13 17:47:16 -0700

    Added daisyUI, removed unused pages

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+    <div>
+      <p>Copyright © 2022 - <a class="link link-accent" href="https://coreydamocles.netlify.app/">Corey Damocles</a></p>
+    </div>
+</footer>
